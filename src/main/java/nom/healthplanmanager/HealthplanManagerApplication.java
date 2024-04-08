6275102ad6d6f7698533e1098041484737d73648
Leonardo Marquini Facchini <lmfacchinni@kami.com>
2024-04-08 20:08:36 -0300

    add spring boot files

---
+++ src/main/java/nom/healthplanmanager/HealthplanManagerApplication.java
@@ -0,0 +1,13 @@
+package nom.healthplanmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthplanManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthplanManagerApplication.class, args);
+	}
+
+}
